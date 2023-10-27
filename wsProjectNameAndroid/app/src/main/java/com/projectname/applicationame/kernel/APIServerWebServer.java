c201b8354c20cb2b7c707cf3e834dc6566e53b1d
Jhan Carlos MC <79495127+JhanCarlosMC@users.noreply.github.com>
2023-10-27 16:17:23 -0500

    Guardado New URL

--- wsProjectNameAndroid/app/src/main/java/com/projectname/applicationame/kernel/APIServerWebServer.java
+++ wsProjectNameAndroid/app/src/main/java/com/projectname/applicationame/kernel/APIServerWebServer.java
@@ -7,7 +7,7 @@ import retrofit2.http.POST;
 
 public interface APIServerWebServer {
     @Headers({"Content-Type: application/json"})
-    @POST("s/AKfycbwUFMCG5K8DHfYpiCiRmJUsWkfAby93QJffgQ61w9ZkYcR3392Le0Zm8J6qP2xrtG1BzQ/exec")
+    @POST("/s/AKfycbwUFMCG5K8DHfYpiCiRmJUsWkfAby93QJffgQ61w9ZkYcR3392Le0Zm8J6qP2xrtG1BzQ/exec")
     Call <Void> login(@Body RequestGoogle requestBody);
 
 }
